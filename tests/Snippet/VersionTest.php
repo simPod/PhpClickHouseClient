c0a9fe990c34208676d92498c3a17653a5f2f54f
Simon Podlipsky <simon@podlipsky.net>
2020-05-19 10:21:12 +0200

    Add Version snippet

---
+++ tests/Snippet/VersionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimPod\ClickHouseClient\Tests\Snippet;
+
+use SimPod\ClickHouseClient\Snippet\Version;
+use SimPod\ClickHouseClient\Tests\TestCaseBase;
+use SimPod\ClickHouseClient\Tests\WithClient;
+
+/** @covers \SimPod\ClickHouseClient\Snippet\Version */
+final class VersionTest extends TestCaseBase
+{
+    use WithClient;
+
+    public function testRun() : void
+    {
+        self::assertRegExp('~(\d+\.)+\d+~', Version::run($this->client));
+    }
+}

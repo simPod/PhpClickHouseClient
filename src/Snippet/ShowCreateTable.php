d52632cafa896022904f5ee659a8b7c3b7ba98f5
Simon Podlipsky <simon@podlipsky.net>
2020-08-12 07:57:23 +0200

    Use JsonEachRow format in ShowCreateTable
    \n"s were escaped with TabSeparated

--- src/Snippet/ShowCreateTable.php
+++ src/Snippet/ShowCreateTable.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace SimPod\ClickHouseClient\Snippet;
 
 use SimPod\ClickHouseClient\Client\ClickHouseClient;
-use SimPod\ClickHouseClient\Format\TabSeparated;
+use SimPod\ClickHouseClient\Format\JsonEachRow;
+use Webmozart\Assert\Assert;
 use function trim;
 
 final class ShowCreateTable
@@ -16,9 +17,12 @@ final class ShowCreateTable
             <<<CLICKHOUSE
 SHOW CREATE TABLE $tableName
 CLICKHOUSE,
-            new TabSeparated()
+            new JsonEachRow()
         );
 
-        return trim($output->contents);
+        $statement = $output->data[0]['statement'];
+        Assert::string($statement);
+
+        return trim($statement);
     }
 }

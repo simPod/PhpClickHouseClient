f4e4274a7431ec4cd8937f2873b18e5aa37bfea3
Simon Podlipsky <simon@podlipsky.net>
2023-02-24 11:12:35 +0100

    feat: allow passing uri to RequestFactory

--- src/Client/Http/RequestFactory.php
+++ src/Client/Http/RequestFactory.php
@@ -7,8 +7,11 @@ namespace SimPod\ClickHouseClient\Client\Http;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
 
 use function http_build_query;
+use function is_string;
 
 use const PHP_QUERY_RFC3986;
 
@@ -17,6 +20,8 @@ final class RequestFactory
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
+        private UriFactoryInterface|null $uriFactory = null,
+        private UriInterface|string $uri = '',
     ) {
     }
 
@@ -29,7 +34,20 @@ final class RequestFactory
             PHP_QUERY_RFC3986,
         );
 
-        return $this->requestFactory->createRequest('POST', $query === '' ? '' : '?' . $query)
+        if ($this->uriFactory === null) {
+            return $this->requestFactory->createRequest('POST', $query === '' ? '' : '?' . $query)
+                ->withBody($this->streamFactory->createStream($requestOptions->sql));
+        }
+
+        $uri = $this->uri;
+        if (is_string($uri)) {
+            $uri = $this->uriFactory->createUri($uri);
+        }
+
+        $uriQuery = $uri->getQuery();
+        $uri      = $uri->withQuery($uriQuery . ($uriQuery !== '' && $query !== '' ? '&' : '') . $query);
+
+        return $this->requestFactory->createRequest('POST', $uri)
             ->withBody($this->streamFactory->createStream($requestOptions->sql));
     }
 }

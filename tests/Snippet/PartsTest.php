2cbdc986c9ad3619536aa5669c7bebfc84844302
Simon Podlipsky <simon@podlipsky.net>
2024-01-17 13:18:27 +0100

    feat(snippet): add database parameter to Parts (#244)

--- tests/Snippet/PartsTest.php
+++ tests/Snippet/PartsTest.php
@@ -4,18 +4,42 @@ declare(strict_types=1);
 
 namespace SimPod\ClickHouseClient\Tests\Snippet;
 
+use DateTimeImmutable;
 use PHPUnit\Framework\Attributes\CoversClass;
 use SimPod\ClickHouseClient\Snippet\Parts;
 use SimPod\ClickHouseClient\Tests\TestCaseBase;
 use SimPod\ClickHouseClient\Tests\WithClient;
+
+use function assert;
+use function is_string;
 
 #[CoversClass(Parts::class)]
 final class PartsTest extends TestCaseBase
 {
     use WithClient;
 
+    public function setUp(): void
+    {
+        self::$client->executeQuery(
+            <<<'CLICKHOUSE'
+CREATE TABLE test (
+    a_date  DateTime,
+    value   Int8
+)
+ENGINE = MergeTree
+    PARTITION BY toDate(a_date)
+    ORDER BY (value)
+CLICKHOUSE,
+        );
+        self::$client->insert('test', [[new DateTimeImmutable('2020-08-01 00:11:22'), 1]]);
+    }
+
     public function testRun(): void
     {
-        self::assertSame([], Parts::run(self::$client, 'system.query_log'));
+        $currentDbName = self::$currentDbName;
+        assert(is_string($currentDbName));
+
+        self::assertCount(1, Parts::run(self::$client, $currentDbName, 'test'));
+        self::assertCount(0, Parts::run(self::$client, $currentDbName, 'test', false));
     }
 }

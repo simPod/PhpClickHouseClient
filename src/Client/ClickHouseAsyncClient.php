3b4afa62744fac4eb2407e1e9a2854efdfb98fca
Simon Podlipsky <simon@podlipsky.net>
2026-06-26 10:37:21 +0300

    refactor(async): return futures from async client

--- src/Client/ClickHouseAsyncClient.php
+++ src/Client/ClickHouseAsyncClient.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace SimPod\ClickHouseClient\Client;
 
 use Amp\Future;
-use GuzzleHttp\Promise\PromiseInterface;
 use SimPod\ClickHouseClient\Format\Format;
 use SimPod\ClickHouseClient\Output\Output;
 use SimPod\ClickHouseClient\Settings\EmptySettingsProvider;
@@ -16,22 +15,11 @@ interface ClickHouseAsyncClient
     /**
      * @param Format<O> $outputFormat
      *
-     * @template O of Output
-     */
-    public function select(
-        string $query,
-        Format $outputFormat,
-        SettingsProvider $settings = new EmptySettingsProvider(),
-    ): PromiseInterface;
-
-    /**
-     * @param Format<O> $outputFormat
-     *
      * @return Future<O>
      *
      * @template O of Output
      */
-    public function selectFuture(
+    public function select(
         string $query,
         Format $outputFormat,
         SettingsProvider $settings = new EmptySettingsProvider(),
@@ -41,24 +29,11 @@ interface ClickHouseAsyncClient
      * @param array<string, mixed>            $params
      * @param Format<O>                       $outputFormat
      *
-     * @template O of Output
-     */
-    public function selectWithParams(
-        string $query,
-        array $params,
-        Format $outputFormat,
-        SettingsProvider $settings = new EmptySettingsProvider(),
-    ): PromiseInterface;
-
-    /**
-     * @param array<string, mixed>            $params
-     * @param Format<O>                       $outputFormat
-     *
      * @return Future<O>
      *
      * @template O of Output
      */
-    public function selectWithParamsFuture(
+    public function selectWithParams(
         string $query,
         array $params,
         Format $outputFormat,
